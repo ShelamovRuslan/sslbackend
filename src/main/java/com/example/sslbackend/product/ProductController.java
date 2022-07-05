315d4740cec77c6e6289a49a0cd8b9df55470052
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-07-05 07:41:49 +0300

    7

--- src/main/java/com/example/sslbackend/product/ProductController.java
+++ src/main/java/com/example/sslbackend/product/ProductController.java
@@ -2,12 +2,12 @@ package com.example.sslbackend.product;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
